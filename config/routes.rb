592576ce458a91c4ecdef642d2487677db128e87
Nick Oki <nick.oki@live.com>
2017-02-23 14:05:29 -0500

    Add validations for User first_name and last_name

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # devise_for :users
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  # Root path
   root 'static#home'
+
+  # Devise routes, controller override
+  devise_for :users, :controllers => { registrations: 'registrations' }
+
+  # Routes for presentations
   resources :presentations
 end
