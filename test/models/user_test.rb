c76ef8171bd9b057881d4ca27f82fb1141fd21c1
Pramod Jacob <psjacob68@gmail.com>
2017-02-24 16:09:48 -0500

    Added presentations model testing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,34 +1,43 @@
 require "test_helper"
+include FactoryGirl::Syntax::Methods
 
 describe User do
+
   before do
-    @user = User.create(
-      email: "nicholas.oki@excella.com",
-      password: "testing",
-      password_confirmation: "testing",
-      first_name: "Nick",
-      last_name: "Oki"
-    )
+    @user = build(:user)
   end
 
-  # Happy Path
   it 'requires a user to be valid' do
     # Arrange
 
     # Act
     result = @user.valid?
+
     # Assert
-    assert result, "User is not valid"
+    assert result, "Valid user was considered invalid"
   end
 
-  # Nil Value
-  it 'requires a user to be valid' do
+  it 'rejects a user without a first name' do
     # Arrange
     @user.first_name = nil
+
     # Act
     result = @user.valid?
+
     # Assert
-    refute result, "first_name cannot be nil"
+    refute result, "Accepted user with invalid first name"
   end
+
+  it 'rejects a user without a last name' do
+    # Arrange
+    @user.last_name = nil
+
+    # Act
+    result = @user.valid?
+
+    # Assert
+    refute result, "Accepted user with invalid last name"
+  end
+
 
 end
