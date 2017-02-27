535c3c46046ab31a8b66ddded8ce7efe9c8f6253
Nick Oki <nick.oki@live.com>
2017-02-27 11:53:36 -0500

    Completed integration test for User Sign Out

---
+++ test/features/sign_out_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class SignOutTest < Capybara::Rails::TestCase
+  include FactoryGirl::Syntax::Methods
+  include Warden::Test::Helpers
+  Warden.test_mode!
+
+  after do
+    Warden.test_reset!
+  end
+
+  feature "Sign Out" do
+    scenario "visits sign out path" do
+      u = create :user
+      login_as(u, scope: :user)
+      # Must refresh page for login_as to take effect
+      visit root_path
+      click_on "Log Out"
+      # Check for "Log In" option in nav
+      page.must_have_content "Log In"
+    end
+  end
+end
