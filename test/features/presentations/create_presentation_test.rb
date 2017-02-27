defbfaf46c564838cc7696f7a43cc9cd94db2b9e
Nick Oki <nick.oki@live.com>
2017-02-27 14:18:01 -0500

    Complete integration test for create new presentation as non-admin

--- test/features/presentations/create_presentation_test.rb
+++ test/features/presentations/create_presentation_test.rb
@@ -25,5 +25,13 @@ class CreatePresentationTest < Capybara::Rails::TestCase
       # Check for "Foo Bar" listing
       page.must_have_content "Foo Bar"
     end
+
+    scenario "redirects to presentation index if non-admin" do
+      u = create :user
+      login_as(u, scope: :user)
+      # Must refresh page for login_as to take effect
+      visit new_presentation_path
+      assert_equal current_path, presentations_path
+    end
   end
 end
