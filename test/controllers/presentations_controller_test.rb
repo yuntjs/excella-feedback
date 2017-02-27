150be9953c4f87e999500e4b6e4abd3dc5ff87b2
Nick Oki <nick.oki@live.com>
2017-02-27 13:59:59 -0500

    Clean up presentations_controller_test.rb

--- test/controllers/presentations_controller_test.rb
+++ test/controllers/presentations_controller_test.rb
@@ -18,31 +18,20 @@ class PresentationsControllerTest < ActionController::TestCase
   describe "#index" do
 
     it "gets all presentations if you are an admin" do
-      # Arrange
       admin = create :user, :admin
       presentation = create :presentation
       sign_in admin
-
-      # Act
       get :index
-
-      # Assert
       assert_equal Presentation.all, [presentation], "Did not return presentations"
     end
 
     it "gets only presentations for which a non-admin user is a participant" do
-      # Arrange
       u = create :user
       pres1 = create :presentation
       pres2 = create :presentation
-      # Add a join bw user and presentation
       part = create :participation, user: u, presentation: pres1
       sign_in u
-
-      # Act
       get :index
-
-      # Assert
       assert_equal u.presentations, [pres1], "Returned presentation for which the user is not a participant"
     end
 
@@ -52,7 +41,6 @@ class PresentationsControllerTest < ActionController::TestCase
   describe "#create" do
 
     it "redirects to index page if logged in" do
-      # Arrange
       user = User.create(
         email: "nicholas.oki@excella.com",
         password: "testing",
@@ -61,21 +49,12 @@ class PresentationsControllerTest < ActionController::TestCase
         last_name: "Oki"
       )
       sign_in user
-
-      # Act
       post :create, params: @params
-
-      # Assert
       assert_redirected_to presentations_path, "Create method unsuccessful, no redirect to presentations_path"
     end
 
     it "redirects to sign in if no current_user" do
-      # Arrange
-
-      # Act
       post :create, params: @params
-
-      # Assert
       assert_redirected_to new_user_session_path
     end
   end
