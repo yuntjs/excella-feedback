20b7c193b719f747f301dad5df2b470105ea2a3b
Nick Oki <nick.oki@live.com>
2017-02-27 15:40:00 -0500

    Add Participation routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,15 @@ Rails.application.routes.draw do
   # Devise routes, controller override
   devise_for :users, :controllers => { registrations: 'registrations' }
 
-  # Routes for presentations
+  # Routes for Presentation
   resources :presentations do
+    # Routes for Participation
+    post 'add_participation'
+    get 'edit_participation'
+    put 'update_participation'
+    delete 'remove_participation'
+
+    # Routes for Survey and Question
     resources :surveys do
       resources :questions
     end
