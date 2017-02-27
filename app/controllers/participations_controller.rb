61cb911ac75c25e8ee306c441eb0165a73dedb4a
Nick Oki <nick.oki@live.com>
2017-02-27 15:38:21 -0500

    rails d controller participations

--- app/controllers/participations_controller.rb
+++
@@ -1,2 +0,0 @@
-class ParticipationsController < ApplicationController
-end
--- test/controllers/participations_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-describe ParticipationsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
-end
