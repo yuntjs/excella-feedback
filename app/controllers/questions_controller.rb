57007c9309d25d768262a988628faa739dd3adcb
Drew Nickerson <dnick13@gmail.com>
2017-02-27 13:47:48 -0500

    Ability to add questions to a survey. Need button for actual user interaction

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,10 +6,23 @@ class QuestionsController < ApplicationController
   end
 
   def create
+    @question = Question.new(question_params)
+    @question.survey_id = params[:survey_id]
+    @presentation = Presentation.find(params[:presentation_id])
 
+    if @question.save
+      redirect_to presentation_survey_path(@presentation.id, @question.survey_id)
+    else
+      render :new
+    end
   end
 
   def show
 
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:order, :prompt, :response_type)
   end
 end
