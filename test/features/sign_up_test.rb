e528996c3a11e89068b0f4110707a71f2d58cd31
Nick Oki <nick.oki@live.com>
2017-02-27 11:17:23 -0500

    Complete ingration test for bad User Sign Up

--- test/features/sign_up_test.rb
+++ test/features/sign_up_test.rb
@@ -3,10 +3,12 @@ require "test_helper"
 class SignUpTest < Capybara::Rails::TestCase
 
   feature "Sign Up" do
-    scenario "has valid content" do
+    before do
       visit root_path
       click_on "Sign Up"
+    end
 
+    scenario "has valid content" do
       within ("form") do
         fill_in "Email", with: "user@example.com"
         fill_in "First name", with: "First"
@@ -15,8 +17,23 @@ class SignUpTest < Capybara::Rails::TestCase
         fill_in "Password confirmation", with: "testing"
         click_button "Sign Up"
       end
-
       assert_equal current_path, root_path
     end
+
+    scenario "had invalid content" do
+      within ("form") do
+        fill_in "Email", with: "user@example.com"
+        fill_in "First name", with: "First"
+        fill_in "Last name", with: "Last"
+        fill_in "Password", with: "password"
+        fill_in "Password confirmation", with: "badpassword"
+        click_button "Sign Up"
+      end
+      page.must_have_content "Password confirmation doesn't match Password"
+    end
   end
+  #
+  # feature "Sign In" do
+  #
+  # end
 end
