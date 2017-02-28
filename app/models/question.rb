350c2063251e934779989032ea947c040e5bfa05
Pramod Jacob <psjacob68@gmail.com>
2017-02-27 23:50:57 -0500

    added dependent destroy and dependent nullify to various models

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :survey
 
-  has_many :responses
-  has_many :users, through: :responses
+  has_many :responses, dependent: :destroy
+  has_many :users, through: :responses, dependent: :destroy
 end
