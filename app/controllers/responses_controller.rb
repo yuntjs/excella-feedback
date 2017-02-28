ca9a970655757397869ed832413bc7ac46fcfadd
Drew Nickerson <dnick13@gmail.com>
2017-02-28 15:06:37 -0500

    Can successfully submit responses and have them recorded. Need logic for ordering surveys and questions. Need validation checks before submitting. Need unit tests.

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -4,6 +4,12 @@ class ResponsesController < ApplicationController
   end
 
   def create
-    byebug
+    all_responses = params[:question]
+    all_responses.each do |question_id, answer|
+      response = Response.new(question_id: question_id, user_id: current_user.id, value: answer)
+      response.save
+    end
+    flash[:notice] = "Answers successfully recorded"
+    redirect_to presentation_path(params[:presentation_id])
   end
 end
