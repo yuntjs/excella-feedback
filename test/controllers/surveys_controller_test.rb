f67bd2f8fb7f7e665ca69cd90cffa9c83d2a2e22
Khoi Le <khoileee89@gmail.com>
2017-02-28 10:14:02 -0500

    Further cleanup

--- test/controllers/surveys_controller_test.rb
+++ test/controllers/surveys_controller_test.rb
@@ -6,16 +6,11 @@ class SurveysControllerTest < ActionController::TestCase
 
     describe '#create' do
       it "Should create a new survey if User is an Admin" do
-        #Arrange
         admin = create :user, :admin
         presentation = create :presentation
 
         sign_in admin
-
-        #Act
         post :create, params: {presentation_id: presentation.id, survey:{ order: 1, subject: "Git"}}
-
-        #Assert
         assert_redirected_to presentation_survey_path(presentation.id, presentation.surveys.first.id), "Create method unsuccessful, no redirect to presentations_survey_path"
 
       end
@@ -23,7 +18,6 @@ class SurveysControllerTest < ActionController::TestCase
 
     describe '#update' do
       it "Should allow Admin user to Update surveys" do
-        #Arrange
         admin = create :user, :admin
         presentation = create :presentation
         survey1 = presentation.surveys.create(order: 1, subject: "Git")
@@ -32,12 +26,7 @@ class SurveysControllerTest < ActionController::TestCase
         updated_order = 2
 
         sign_in admin
-
-        #Act
-
         patch :update, params: {presentation_id: presentation.id, id: presentation.surveys.first.id, survey:{order: updated_order, subject: updated_subject}}
-
-        #Assert
         assert_redirected_to presentation_survey_path(presentation.id, presentation.surveys.first.id)
 
       end
