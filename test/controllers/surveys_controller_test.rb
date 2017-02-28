96075a9e7f464f0ae37d578712944ec64f8ca166
Khoi Le <khoileee89@gmail.com>
2017-02-28 15:36:54 -0500

    Minor cleanup

--- test/controllers/surveys_controller_test.rb
+++ test/controllers/surveys_controller_test.rb
@@ -10,6 +10,7 @@ class SurveysControllerTest < ActionController::TestCase
         presentation = create :presentation
 
         sign_in admin
+        
         post :create, params: {presentation_id: presentation.id, survey:{ order: 1, subject: "Git"}}
         assert_redirected_to presentation_survey_path(presentation.id, presentation.surveys.first.id), "Create method unsuccessful, no redirect to presentations_survey_path"
 
@@ -21,14 +22,12 @@ class SurveysControllerTest < ActionController::TestCase
         admin = create :user, :admin
         presentation = create :presentation
         survey = create :survey, order: 1, subject: "Git", presentation_id: presentation.id
-
         updated_subject = "Git 2"
         updated_order = 2
 
         sign_in admin
 
         patch :update, params: {presentation_id: presentation.id, id: survey.id, survey:{order: updated_order, subject: updated_subject}}
-
         survey.reload
 
         assert_equal [updated_subject, updated_order], [survey.subject, survey.order], "Update method unsuccessful, attributes are not equal"
@@ -45,7 +44,6 @@ class SurveysControllerTest < ActionController::TestCase
         sign_in admin
 
         delete :destroy, params: {presentation_id: presentation.id, id: survey.id}
-
         assert_equal Survey.count, 0, "Delete method unsuccessful. Survey still exists."
       end
     end
