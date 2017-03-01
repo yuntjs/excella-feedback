c110b943ae80824ef9b16130beb95bf2224d189b
Pramod Jacob <psjacob68@gmail.com>
2017-03-01 10:54:46 -0500

    extracted out logic in presentations/table partial into helper

--- app/helpers/presentations_helper.rb
+++ app/helpers/presentations_helper.rb
@@ -1,20 +1,43 @@
 module PresentationsHelper
 
+  def feedback_header(is_admin)
+    is_admin ? 'Admin' : 'Feedback'
+  end
+
   def display_description(presentation)
     if presentation.description.length > 30
-      content_tag(:span, presentation.description_short(30)) + 
-      content_tag(:a, '(more)',
-        tabindex: '0',
-        role: 'button',
-        data: {
-          toggle: 'popover',
-          placement: 'bottom',
-          trigger: 'focus',
-          content: presentation.description
-        }
-      )
+      description = content_tag(:span, presentation.description_short(30))
+      view_link = content_tag(:a, '(more)', tabindex: '0', role: 'button',
+                    data: {
+                      toggle: 'popover',
+                      placement: 'bottom',
+                      trigger: 'focus',
+                      content: presentation.description
+                    }
+                  )
+      description + view_link
     else
       presentation.description
+    end
+  end
+
+  def feedback_content(is_admin, presentation, feedback_message)
+    if is_admin
+      edit_link = link_to "Edit", edit_presentation_path(presentation), class: 'btn btn-default'
+      delete_link = link_to "Delete", presentation_path(presentation), class: "btn btn-default", method: :delete
+      survey_link = survey_link_for(presentation)
+
+      edit_link + delete_link + survey_link
+    else
+      link_to feedback_message
+    end
+  end
+
+  def survey_link_for(presentation)
+    if presentation.surveys.any?
+      link_to "See Surveys", presentation_surveys_path(presentation)
+    else
+      link_to "Create Survey", new_presentation_survey_path(presentation)
     end
   end
 
