4768ac9ec9fc880c662ccadaa625c102ff9acb59
Nick Oki <nick.oki@live.com>
2017-03-01 15:19:02 -0500

    Fix integration test bug, complete integration test for destroying participation

--- app/assets/javascripts/presentations.js
+++ app/assets/javascripts/presentations.js
@@ -2,8 +2,6 @@
 // All this logic will automatically be available in application.js.
 
 $(function () {
-  // Toggle participation form modal
-  $('[data-toggle="popover"]').popover()
   // Event listener for participation form submit
   $('#participation-form-submit').on('click', function() {
     $('#participation-form').submit()
--- test/features/participations/destroy_participation_test.rb
+++ test/features/participations/destroy_participation_test.rb
@@ -1,5 +1,4 @@
 require "test_helper"
-require "capybara/rails"
 
 class CreateParticipationTest < Capybara::Rails::TestCase
   include Warden::Test::Helpers
@@ -9,28 +8,30 @@ class CreateParticipationTest < Capybara::Rails::TestCase
     Warden.test_reset!
   end
 
-  feature "Create" do
-    scenario "creates a new participation if admin" do
+  feature "Destroy" do
+    scenario "destroys a participation if admin" do
       u = create :user, :admin
       pres = create(:presentation, title: "user's presentation")
+      create(:participation, user: u, presentation: pres)
       login_as(u, scope: :user)
       # Must refresh page for login_as to take effect
       visit presentation_path(pres)
 
       within "#participation-form-modal" do
-        page.check u.email
+        page.uncheck u.email
       end
 
       find("#submit-capybara", visible: false).click
 
       within "ol" do
-        page.must_have_content u.email
+        refute page.has_content? u.email
       end
     end
 
-    scenario "unable to create a new participation if non-admin" do
+    scenario "unable to destroy a participation if non-admin" do
       u = create :user
       pres = create(:presentation, title: "user's presentation")
+      create(:participation, user: u, presentation: pres)
       login_as(u, scope: :user)
       # Must refresh page for login_as to take effect
       visit presentation_path(pres)
