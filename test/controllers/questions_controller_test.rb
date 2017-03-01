a656ed60bb398375d4bb43ce1bb03699897f8e9e
Khoi Le <khoileee89@gmail.com>
2017-03-01 09:48:56 -0500

    created seperate assertion test for redirect under Update(questions controller)

--- test/controllers/questions_controller_test.rb
+++ test/controllers/questions_controller_test.rb
@@ -32,7 +32,18 @@ class QuestionsControllerTest < ActionController::TestCase
       question.reload
 
       assert_equal question.prompt, updated_prompt, "Update unsuccessful, values do not match"
-      assert_redirected_to presentation_survey_path(presentation.id, survey.id)
+    end
+    it "Should redirect to correct path after an update" do
+      admin = create :user, :admin
+      presentation = create :presentation
+      survey = create :survey, order: 1, subject: "Git", presentation_id: presentation.id
+      question = create :question, survey_id: survey.id
+      updated_prompt = "Feedback is an internal application that allows?"
+
+      sign_in admin
+
+      patch :update, params: {presentation_id: presentation.id, survey_id: survey.id, id: question.id, question:{prompt: updated_prompt}}
+      assert_redirected_to presentation_survey_path(presentation.id, survey.id), "Update unsucessful, no redirect"
     end
   end
 end
