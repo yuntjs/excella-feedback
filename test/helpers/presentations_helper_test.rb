2106c8548fec498f12ab1bc3223d47be0d8a2b58
Pramod Jacob <psjacob68@gmail.com>
2017-03-02 11:09:40 -0500

    completed unit test for presentations_as

--- test/helpers/presentations_helper_test.rb
+++ test/helpers/presentations_helper_test.rb
@@ -1,12 +1,8 @@
 require 'test_helper'
 
 class PresentationsHelperTest < ActionView::TestCase
-  # include Warden::Test::Helpers
-  # Warden.test_mode!
-
   before do
     @user = create(:user)
-    # login_as(@user, scope: :user)
 
     @presentation_as_presenter = create(:presentation)
     @presentation_as_attendee = create(:presentation)
@@ -23,17 +19,27 @@ class PresentationsHelperTest < ActionView::TestCase
     )
   end
 
-  # after do
-  #   Warden.test_reset!
-  # end
-
   describe '#presentations_as' do
     it 'gets correct presentations when current user is presenter' do
-      assert_equal presentations_as(:presenter, @user), [@presentation_as_presenter]
+      presentations = presentations_as(:presenter, @user)
+      assert_equal presentations, [@presentation_as_presenter]
     end
 
-    it 'gets correct presentations where current user is attendee'
-    it 'returns an empty relation is role is not specified as presenter or attendee'
-    it 'raises an error if role is not specified'
+    it 'gets correct presentations where current user is attendee' do
+      presentations = presentations_as(:attendee, @user)
+      assert_equal presentations, [@presentation_as_attendee]
+    end
+
+    it 'returns an empty relation is role is not specified as presenter or attendee' do
+      assert_equal presentations_as(:another_role, @user), []
+    end
+
+    it 'raises an error if role is not specified' do
+      assert_raises(ArgumentError) { presentations_as(@user) }
+    end
+
+    it 'raises an error if user is not specified' do
+      assert_raises(ArgumentError) { presentations_as(:presenter) }
+    end
   end
 end
