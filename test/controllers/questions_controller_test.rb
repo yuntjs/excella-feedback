d84a0d3df8069ae496e31384cbe8e20c095d440a
Khoi Le <khoileee89@gmail.com>
2017-03-02 13:47:21 -0500

    DRYing up Unit test for questions-controller

--- test/controllers/questions_controller_test.rb
+++ test/controllers/questions_controller_test.rb
@@ -4,46 +4,41 @@ class QuestionsControllerTest < ActionController::TestCase
   include Devise::Test::ControllerHelpers
   include FactoryGirl::Syntax::Methods
 
+  before do
+    @admin = create :user, :admin
+    @presentation = create :presentation
+    @survey = create :survey, order: 1, subject: "Git", presentation_id: @presentation.id
+    @question = create :question, survey_id: @survey.id
+  end
+
   describe '#create' do
     it "Should create a new question if User is an Admin" do
-      admin = create :user, :admin
-      presentation = create :presentation
-      survey = create :survey, order: 1, subject: "Git", presentation_id: presentation.id
       question = create :question
 
-      sign_in admin
+      sign_in @admin
 
-      post :create, params: { presentation_id: presentation.id, survey_id: survey.id, question:{ order: question.order, prompt: question.prompt, response_type: question.response_type } }
-      assert_redirected_to presentation_survey_path(presentation.id, survey.id), "Create method unsuccessful, no redirect to presentation_survey_path"
+      post :create, params: { presentation_id: @presentation.id, survey_id: @survey.id, question:{ order: question.order, prompt: question.prompt, response_type: question.response_type } }
+      assert_redirected_to presentation_survey_path(@presentation.id, @survey.id), "Create method unsuccessful, no redirect to presentation_survey_path"
     end
   end
 
   describe '#update' do
     it "Should allow admin to update questions" do
-      admin = create :user, :admin
-      presentation = create :presentation
-      survey = create :survey, order: 1, subject: "Git", presentation_id: presentation.id
-      question = create :question, survey_id: survey.id
       updated_prompt = "Feedback is an internal application that allows?"
 
-      sign_in admin
+      sign_in @admin
 
-      patch :update, params: {presentation_id: presentation.id, survey_id: survey.id, id: question.id, question:{prompt: updated_prompt}}
-      question.reload
-
-      assert_equal question.prompt, updated_prompt, "Update unsuccessful, values do not match"
+      patch :update, params: {presentation_id: @presentation.id, survey_id: @survey.id, id: @question.id, question:{prompt: updated_prompt}}
+      @question.reload
+      assert_equal @question.prompt, updated_prompt, "Update unsuccessful, values do not match"
     end
     it "Should redirect to correct path after an update" do
-      admin = create :user, :admin
-      presentation = create :presentation
-      survey = create :survey, order: 1, subject: "Git", presentation_id: presentation.id
-      question = create :question, survey_id: survey.id
       updated_prompt = "Feedback is an internal application that allows?"
 
-      sign_in admin
+      sign_in @admin
 
-      patch :update, params: {presentation_id: presentation.id, survey_id: survey.id, id: question.id, question:{prompt: updated_prompt}}
-      assert_redirected_to presentation_survey_path(presentation.id, survey.id), "Update unsucessful, no redirect"
+      patch :update, params: {presentation_id: @presentation.id, survey_id: @survey.id, id: @question.id, question:{prompt: updated_prompt}}
+      assert_redirected_to presentation_survey_path(@presentation.id, @survey.id), "Update unsucessful, no redirect"
     end
   end
 end
