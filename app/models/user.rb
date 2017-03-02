87700a5076d7c03d827a41b8c5882ef57d7cc548
Nick Oki <nick.oki@live.com>
2017-03-02 15:20:58 -0500

    Add full_name method

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,8 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  def full_name
+    full_name = "#{first_name} #{last_name}"
+  end
 end
