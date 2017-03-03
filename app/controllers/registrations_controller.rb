dae1a774c8c25343e289b4a9b84f544ee8a73958
Pramod Jacob <psjacob68@gmail.com>
2017-03-03 10:45:14 -0500

    added flash messages to survey controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,4 @@
 class RegistrationsController < Devise::RegistrationsController
-
   def create
     super
   end
@@ -13,5 +12,4 @@ class RegistrationsController < Devise::RegistrationsController
     def account_update_params
       params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
     end
-
 end
