efa487d02f4edaf815ce426e83526f3197cd540a
Pramod Jacob <psjacob68@gmail.com>
2017-03-03 14:36:37 -0500

    added flash messages to create presentation test

--- test/features/presentations/create_presentation_test.rb
+++ test/features/presentations/create_presentation_test.rb
@@ -25,6 +25,25 @@ class CreatePresentationTest < Capybara::Rails::TestCase
       end
 
       page.must_have_content "Foo Bar"
+      page.must_have_content "Success! Presentation has been successfully created."
+    end
+
+    scenario "does not create a presentation if it is invalid" do
+      user = create :user, :admin
+      login_as(user, scope: :user)
+
+      visit root_path
+
+      click_on "Create New Presentation"
+
+      within ("form") do
+        fill_in "Title", with: ""
+        fill_in "Location", with: ""
+        fill_in "Description", with: ""
+        click_button "Submit"
+      end
+
+      page.must_have_content "Warning! We ran into some errors while trying to create this presentation. Please try again."
     end
 
     scenario "redirects to presentation index if non-admin" do
