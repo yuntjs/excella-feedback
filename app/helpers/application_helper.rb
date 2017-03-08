ffc0108f661ab8f054293bd03d52eedb74420476
Nick Oki <nick.oki@live.com>
2017-03-08 09:57:49 -0500

    Add back button to views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
   # Helper to render a uniform back button
   # Defaults to :back path if none is specified
   #
-  def back_button(path: :back)
+  def back_button(path = :back)
     link_to path, class: "btn btn-default" do
       content_tag(:span, "", class: "glyphicon glyphicon-chevron-left", 'aria-hidden': "true") + "&nbsp;Back".html_safe
     end
