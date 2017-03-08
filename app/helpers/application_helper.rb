1b5d90980c7201263592f163151e5b9abea05340
Nick Oki <nick.oki@live.com>
2017-03-08 09:43:39 -0500

    Reformat back_button helper to accept a path as an optional argument

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,8 @@ module ApplicationHelper
     date.strftime("%a - %-m/%d/%y @ %l:%M %P")
   end
 
-  def back_button
-    link_to :back, class: "btn btn-default" do
+  def back_button(path: :back)
+    link_to path, class: "btn btn-default" do
       content_tag(:span, "", class: "glyphicon glyphicon-chevron-left", 'aria-hidden': "true") + "&nbsp;Back".html_safe
     end
   end
