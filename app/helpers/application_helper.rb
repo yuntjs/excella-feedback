caa7a83320d6b85254dcedc1d4268760a09a0368
Nick Oki <nick.oki@live.com>
2017-03-08 09:48:43 -0500

    Write comment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,10 @@ module ApplicationHelper
     date.strftime("%a - %-m/%d/%y @ %l:%M %P")
   end
 
+  #
+  # Helper to render a uniform back button
+  # Defaults to :back path if none is specified
+  #
   def back_button(path: :back)
     link_to path, class: "btn btn-default" do
       content_tag(:span, "", class: "glyphicon glyphicon-chevron-left", 'aria-hidden': "true") + "&nbsp;Back".html_safe
