11b7fe6501dfdb91402ab514c1548bfa0540151e
Nick Oki <nick.oki@live.com>
2017-03-09 14:44:00 -0500

    Add acts_as_list to Survey model

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,10 +1,19 @@
 class Survey < ApplicationRecord
   belongs_to :presentation
 
+  #
+  # acts_as_list gem handles Survey order based on parent Presentation
+  #
+  acts_as_list scope: :presentation
+
   has_many :questions, dependent: :destroy
 
   validates :order, presence: true
   validates :subject, presence: true
+
+  # survey_list = Presentation.find()
+  # survey_list.surveys.first.move_to_bottom
+  # survey_list.surveys.last.move_higher
 
   def order_questions
     self.questions.sort_by {|q| q.order}
