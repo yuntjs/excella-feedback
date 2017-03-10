3d6e1d370f7c6ea18ed491bcb67971f83c03355d
Khoi Le <khoileee89@gmail.com>
2017-03-10 13:43:52 -0500

    Refactor double quote issues features/edit_participation_test

--- test/features/participations/edit_participation_test.rb
+++ test/features/participations/edit_participation_test.rb
@@ -1,5 +1,5 @@
-require "test_helper"
-require "capybara/rails"
+require 'test_helper'
+require 'capybara/rails'
 
 class EditParticipationTest < Capybara::Rails::TestCase
   include Warden::Test::Helpers
@@ -9,8 +9,8 @@ class EditParticipationTest < Capybara::Rails::TestCase
     Warden.test_reset!
   end
 
-  feature "Edit" do
-    scenario "sets attendee to presenter" do
+  feature 'Edit' do
+    scenario 'sets attendee to presenter' do
       admin = create :user, :admin
       pres = create(:presentation, title: "user's presentation")
       part = create(:participation, user: admin, presentation: pres, is_presenter: false)
@@ -18,16 +18,16 @@ class EditParticipationTest < Capybara::Rails::TestCase
       # Must refresh page for login_as to take effect
       visit presentation_path(pres)
 
-      within ".attendees" do
-        click_on "Change to Presenter"
+      within '.attendees' do
+        click_on 'Change to Presenter'
       end
 
-      within ".presenters" do
+      within '.presenters' do
         page.must_have_content admin.email
       end
     end
 
-    scenario "sets presenter to attendee" do
+    scenario 'sets presenter to attendee' do
       admin = create :user, :admin
       pres = create(:presentation, title: "user's presentation")
       part = create(:participation, user: admin, presentation: pres, is_presenter: true)
@@ -35,11 +35,11 @@ class EditParticipationTest < Capybara::Rails::TestCase
       # Must refresh page for login_as to take effect
       visit presentation_path(pres)
 
-      within ".presenters" do
-        click_on "Change to Attendee"
+      within '.presenters' do
+        click_on 'Change to Attendee'
       end
 
-      within ".presenters" do
+      within '.presenters' do
         refute page.has_content? admin.email
       end
     end
