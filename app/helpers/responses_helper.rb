ec1c4ba5ad2ab2fad89f67cef8780e37edbb3a90
Pramod Jacob <psjacob68@gmail.com>
2017-03-10 11:38:28 -0500

    working on creating form with multiple response objects

--- app/helpers/responses_helper.rb
+++ app/helpers/responses_helper.rb
@@ -1,11 +1,11 @@
 module ResponsesHelper
   # Determine which partial to render based on a question's response type
-  def display_question(response)
+  def display_question(response, response_form)
     case response.question.response_type
     when 'text'
-      render 'text_form_partial', response: response
+      render 'text_form_partial', response: response, response_form: response_form
     when 'number'
-      render 'scale_form_partial', response: response
+      # render 'scale_form_partial', response: response, response_form: response_form
     else
     end
   end
