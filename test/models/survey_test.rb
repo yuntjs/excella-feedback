3da0d7eaa3253f1d913b45e575e852f2f5040fe6
Khoi Le <khoileee89@gmail.com>
2017-03-10 13:24:49 -0500

    Added space after colon, test/model survey_test

--- test/models/survey_test.rb
+++ test/models/survey_test.rb
@@ -8,9 +8,9 @@ describe Survey do
 
   describe '#order_questions' do
     it 'orders questions from 0 to n' do
-      question3 = create(:question, survey_id: @survey.id, order:2)
-      question1 = create(:question, survey_id: @survey.id, order:0)
-      question2 = create(:question, survey_id: @survey.id, order:1)
+      question3 = create(:question, survey_id: @survey.id, order: 2)
+      question1 = create(:question, survey_id: @survey.id, order: 0)
+      question2 = create(:question, survey_id: @survey.id, order: 1)
 
       expected_order = [question1, question2, question3]
       actual_order = @survey.order_questions
