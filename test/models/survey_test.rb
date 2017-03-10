68f5936a49520100de0a59c4ecc9a4413404afb0
Khoi Le <khoileee89@gmail.com>
2017-03-10 13:23:42 -0500

    Refactor double quote issues in test/models survey_test

--- test/models/survey_test.rb
+++ test/models/survey_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 include FactoryGirl::Syntax::Methods
 
 describe Survey do
@@ -6,8 +6,8 @@ describe Survey do
     @survey = create(:survey)
   end
 
-  describe "#order_questions" do
-    it "orders questions from 0 to n" do
+  describe '#order_questions' do
+    it 'orders questions from 0 to n' do
       question3 = create(:question, survey_id: @survey.id, order:2)
       question1 = create(:question, survey_id: @survey.id, order:0)
       question2 = create(:question, survey_id: @survey.id, order:1)
@@ -15,7 +15,7 @@ describe Survey do
       expected_order = [question1, question2, question3]
       actual_order = @survey.order_questions
 
-      assert_equal expected_order, actual_order, "Array not ordered correctly"
+      assert_equal expected_order, actual_order, 'Array not ordered correctly'
     end
   end
 end
