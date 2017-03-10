01f7f8477d9a7ed7c019a7cecb4697f403e1cd97
Khoi Le <khoileee89@gmail.com>
2017-03-10 13:48:31 -0500

    Refactor double quote issues in features/authentication

--- test/features/authentication/sign_out_test.rb
+++ test/features/authentication/sign_out_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class SignOutTest < Capybara::Rails::TestCase
   include Warden::Test::Helpers
@@ -8,17 +8,17 @@ class SignOutTest < Capybara::Rails::TestCase
     Warden.test_reset!
   end
 
-  feature "Sign Out" do
-    scenario "visits sign out path" do
+  feature 'Sign Out' do
+    scenario 'visits sign out path' do
       user = create :user
       login_as(user, scope: :user)
 
       # Must refresh page for login_as to take effect
       visit root_path
-      click_on "Log Out"
+      click_on 'Log Out'
 
-      # Check for "Log In" option in nav
-      page.must_have_content "Log In"
+      # Check for 'Log In' option in nav
+      page.must_have_content 'Log In'
     end
   end
 end
