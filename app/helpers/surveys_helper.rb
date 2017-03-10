2b1e0dc4b1fd52b8428990895d9061b5d76f8f55
Khoi Le <khoileee89@gmail.com>
2017-03-10 14:40:30 -0500

    Refactor Surveys helper

--- app/helpers/surveys_helper.rb
+++ app/helpers/surveys_helper.rb
@@ -3,8 +3,7 @@ module SurveysHelper
   def survey_admin_options(user, presentation)
     if user.is_admin
       content_tag :div, class: "admin-options" do
-        new_survey_link = link_to 'Create New Survey', new_presentation_survey_path(presentation), class: "btn btn-primary"
-        new_survey_link
+        link_to 'Create New Survey', new_presentation_survey_path(presentation), class: "btn btn-primary"
       end
     end
   end
