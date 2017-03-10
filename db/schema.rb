623b17c9ac0aadb5730037443d327bd1295662ca
Khoi Le <khoileee89@gmail.com>
2017-03-10 14:55:08 -0500

    Added position column to questions, as well as replaced order with position in seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170310194524) do
+ActiveRecord::Schema.define(version: 20170310195136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20170310194524) do
     t.string   "response_type"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.integer  "position"
     t.index ["survey_id"], name: "index_questions_on_survey_id", using: :btree
   end
 
