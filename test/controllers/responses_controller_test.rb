7a26aceeff57fed038d0b8fa73586740c7aa85c1
Drew Nickerson <dnick13@gmail.com>
2017-03-13 16:15:25 -0400

    Testing for Response Controller #new and #create functions

--- test/controllers/responses_controller_test.rb
+++ test/controllers/responses_controller_test.rb
@@ -9,14 +9,53 @@ class ResponsesControllerTest < ActionController::TestCase
     @presentation = create :presentation
     @survey = create :survey, order: 1, subject: 'Git', presentation_id: @presentation.id
     @questions = create_list :question, 5, :required, :number, survey_id: @survey.id
+
     sign_in @user
   end
 
   describe '#new' do
-    it 'creates multiple response objects from questions for the view' do
+    it 'creates multiple unsaved response objects from questions for the view' do
       get :new, params: { presentation_id: @presentation.id }
+
       assert_equal @questions.length, assigns(:responses).length
     end
+  end
+
+  describe '#create' do
+    it 'creates responses if they are valid' do
+      create(:participation,
+        user_id: @user.id,
+        presentation_id: @presentation.id
+      )
+
+      question_value_pairs = {}
+      @questions.each do |question|
+        question_value_pairs[question.id.to_s] = '1'
+      end
+
+      post :create, params: {
+        presentation_id: @presentation.id,
+        responses: {
+          question_id: question_value_pairs
+        }
+      }
+
+      assert_equal @questions.length, Response.all.length, 'Did not create a response for each question'
+      assert_redirected_to presentation_path(@presentation), 'No redirect to presentation_path'
+    end
+
+    it 'does not save any responses if any are invalid' do
+      question_value_pairs = {}
+      @questions.each do |question|
+        question_value_pairs[question.id.to_s] = '1'
+      end
+
+      question_value_pairs[@questions.first.id.to_s] = ''
+      question_value_pairs[@questions.last.id.to_s] = ''
+
+      assert Response.all.empty?, 'Invalid responses created'
+    end
+
   end
 
 #MOVE TO MODEL TESTS
