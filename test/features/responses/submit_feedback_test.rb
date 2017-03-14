9572d6e4b4708464e5f7434e1588ea061aae8b60
Pramod Jacob <psjacob68@gmail.com>
2017-03-14 16:18:52 -0400

    fixed issue where feedback questions weren't showing

--- test/features/responses/submit_feedback_test.rb
+++ test/features/responses/submit_feedback_test.rb
@@ -5,20 +5,20 @@ class SubmitFeedbackTest < Capybara::Rails::TestCase
   Warden.test_mode!
 
   before do
-    user = create(:user)
-    presentation = create(:presentation)
+    @user = create(:user)
+    @presentation = create(:presentation)
     create(:participation,
-      user_id: user.id,
-      presentation_id: presentation.id
+      user_id: @user.id,
+      presentation_id: @presentation.id
     )
 
-    survey = create(:survey)
-    create_list(:question, 5, :number, survey_id: survey.id)
-    create_list(:question, 5, :text, survey_id: survey.id)
+    @survey = create(:survey, presentation_id: @presentation.id)
+    create_list(:question, 5, :number, survey_id: @survey.id)
+    create_list(:question, 5, :text, survey_id: @survey.id)
 
-    login_as(user, scope: :user)
+    login_as(@user, scope: :user)
 
-    visit new_presentation_response_path(presentation)
+    visit new_presentation_response_path(@presentation)
   end
 
   after do
@@ -27,7 +27,7 @@ class SubmitFeedbackTest < Capybara::Rails::TestCase
 
   feature 'submitting feedback' do
     scenario 'records responses if feedback submission is valid' do
-      
+      save_and_open_page
     end
   end
 end
