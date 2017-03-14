00a4fb7661c310bc2a2cbdb0d9e35a84fa19620e
Nick Oki <nick.oki@live.com>
2017-03-14 12:37:09 -0400

    Refactor create_default_survey method

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -9,12 +9,11 @@ class ParticipationsController < ApplicationController
   # Update route
   #
   def update
-    # binding.pry
     presentation = Presentation.find(@participation.presentation_id)
     presenter = User.find(@participation.user_id)
     # If creating new presenter, create a survey for the presenter
     if participation_params[:is_presenter] == 'true' # Why is this a string?
-      create_default_presenter_survey(presenter)
+      create_default_survey(presenter)
     elsif presentation.surveys.where(presenter_id: presenter.id).exists?
       # Otherwise, delete the presenter's survey(s)
       survey = presentation.surveys.where(presenter_id: presenter.id) # Returns array
@@ -40,18 +39,6 @@ class ParticipationsController < ApplicationController
   # Set and sanitaize participation params
   #
   def participation_params
-    params.require(:participation)
-          .permit(:user_id, :presentation_id, :is_presenter)
-  end
-
-  #
-  # Create default survey for presentation
-  # Default questions in Question Model
-  #
-  def create_default_presenter_survey(presenter)
-    survey = Presentation.find(@participation.presentation_id).surveys.create(subject: "Feedback for #{presenter.full_name}", presenter_id: presenter.id)
-    Question.default_presenter_questions(presenter).each do |question|
-      survey.questions.create(prompt: question[:prompt], response_type: question[:response_type])
-    end
+    params.require(:participation).permit(:user_id, :presentation_id, :is_presenter)
   end
 end
