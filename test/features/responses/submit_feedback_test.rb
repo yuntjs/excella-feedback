20e71f4c68b96332456a2e829e47566033e806e5
Pramod Jacob <psjacob68@gmail.com>
2017-03-15 10:30:14 -0400

    added feature test for invalid feedback submissions

--- test/features/responses/submit_feedback_test.rb
+++ test/features/responses/submit_feedback_test.rb
@@ -1,4 +1,5 @@
 require 'test_helper'
+include FactoryGirl::Syntax::Methods
 
 class SubmitFeedbackTest < Capybara::Rails::TestCase
   include Warden::Test::Helpers
@@ -26,7 +27,7 @@ class SubmitFeedbackTest < Capybara::Rails::TestCase
   end
 
   feature 'submitting feedback' do
-    scenario 'records responses if feedback submission is valid' do
+    scenario 'feedback submission is valid' do
       Question.all.each do |question|
         case question.response_type
         when 'number'
@@ -39,11 +40,32 @@ class SubmitFeedbackTest < Capybara::Rails::TestCase
 
       click_on('Submit')
 
-      assert page.has_content?('Success! Your responses have beeen successfully recorded.'),
+      assert page.has_content?('Success!'),
         'Page does not have success flash message'
       assert_equal Response.count, Question.count,
         'The correct amount of responses were not created'
       assert_equal current_path, presentation_path(@presentation),
+        "Current path does not match desired path"
+    end
+
+    scenario 'feedback submission is invalid' do
+      question = Question.first
+
+      case question.response_type
+      when 'number'
+        choose("responses_question_id_#{question.id}_3")
+      when 'text'
+        fill_in("responses_question_id_#{question.id}", with: "Text")
+      else
+      end
+
+      click_on('Submit')
+
+      assert page.has_content?('Warning!'),
+        'Page does not have error flash message'
+      assert_equal Response.count, 0,
+        'Response were saved after an invalid submission'
+      assert_equal current_path, new_presentation_response_path(@presentation),
         "Current path does not match desired path"
     end
   end
