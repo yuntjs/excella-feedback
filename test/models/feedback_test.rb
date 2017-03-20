6a6ca01b705a25cd71d0de9cbf9031e1c3a10d94
Pramod Jacob <psjacob68@gmail.com>
2017-03-20 14:38:25 -0400

    modified survey in factories

---
+++ test/models/feedback_test.rb
@@ -0,0 +1,15 @@
+#
+# Feedback PORO test
+#
+require 'test_helper'
+
+describe Feedback do
+  before do
+    user = create(:user)
+    # surveys = create
+  end
+
+  describe '#responses' do
+    # it ''
+  end
+end
