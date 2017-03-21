d0c2093b337f0713bae34e8a654d3822738f8668
Nick Oki <nick.oki@live.com>
2017-03-21 10:38:55 -0400

    Rubocop fix ABC size for Participations#update

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -11,14 +11,7 @@ class ParticipationsController < ApplicationController
   # Update route
   #
   def update
-    # If creating new presenter, create a survey for the presenter
-    if participation_params[:is_presenter] == 'true' # Why is this a string?
-      new_default_survey(@presenter)
-    elsif @presentation.surveys.where(presenter_id: @presenter.id).exists?
-      # Otherwise, delete the presenter's survey(s)
-      survey = @presentation.surveys.where(presenter_id: @presenter.id) # Returns array
-      survey.destroy_all
-    end
+    handle_participation_survey
     if @participation.update(participation_params)
       redirect_to presentation_path(params[:presentation_id]), notice: 'Participation was successfully updated.'
     else
@@ -54,5 +47,19 @@ class ParticipationsController < ApplicationController
   #
   def participation_params
     params.require(:participation).permit(:user_id, :presentation_id, :is_presenter)
+  end
+
+  #
+  # Create or Delete surveys when creating new participations
+  #
+  def handle_participation_survey
+    # If creating new presenter, create a survey for the presenter
+    if participation_params[:is_presenter] == 'true' # Why is this a string?
+      new_default_survey(@presenter)
+    elsif @presentation.surveys.where(presenter_id: @presenter.id).exists?
+      # Otherwise, delete the presenter's survey(s)
+      survey = @presentation.surveys.where(presenter_id: @presenter.id) # Returns array
+      survey.destroy_all
+    end
   end
 end
