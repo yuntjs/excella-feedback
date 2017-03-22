49e3abfb45b8dcf785d9033da4d934c0d08c3720
Nick Oki <nick.oki@live.com>
2017-03-22 15:24:21 -0400

    Add private methods to survey_templates_controller

--- app/controllers/survey_templates_controller.rb
+++ app/controllers/survey_templates_controller.rb
@@ -3,6 +3,8 @@
 #
 class SurveyTemplatesController < ApplicationController
   before_action :authenticate_admin
+  before_action :set_survey_template, only: [:show]
+
   #
   # Render Survey Template data
   #
@@ -14,6 +16,21 @@ class SurveyTemplatesController < ApplicationController
   # Show route
   #
   def show
+  end
+
+  private
+
+  #
+  # Set survey_template to be used in routes
+  #
+  def set_survey_template
     @survey_template = SurveyTemplate.find(params[:id])
+  end
+
+  #
+  # Set and sanitize survey params
+  #
+  def survey_template_params
+    params.require(:survey_template).permit(:title, :name)
   end
 end
