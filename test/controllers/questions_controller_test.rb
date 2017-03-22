1285bd847f2b88eb327a4839eecdc7751e2b183e
Pramod Jacob <psjacob68@gmail.com>
2017-03-22 16:12:25 -0400

    reconfigured questions_controller_test.rb to add parentheses

--- test/controllers/questions_controller_test.rb
+++ test/controllers/questions_controller_test.rb
@@ -4,20 +4,20 @@ class QuestionsControllerTest < ActionController::TestCase
   include Devise::Test::ControllerHelpers
 
   before do
-    @admin = create :user, :admin
+    @admin = create(:user, :admin)
 
-    sign_in @admin
+    sign_in(@admin)
 
-    @presentation = create :presentation
-    @survey = create :survey, position: 1, title: 'Git', presentation_id: @presentation.id
-    @question = create :question, survey_id: @survey.id
+    @presentation = create(:presentation)
+    @survey = create(:survey, position: 1, title: 'Git', presentation_id: @presentation.id)
+    @question = create(:question, survey_id: @survey.id)
   end
 
   describe '#create' do
     it 'should create a new question if User is an Admin' do
-      question = create :question
+      question = create(:question)
 
-      post :create, params: {
+      post(:create, params: {
         presentation_id: @presentation.id,
         survey_id: @survey.id,
         question: {
@@ -25,7 +25,7 @@ class QuestionsControllerTest < ActionController::TestCase
           prompt: question.prompt,
           response_type: question.response_type
         }
-      }
+      })
 
       assert_redirected_to presentation_survey_path(@presentation.id, @survey.id), 'No redirect to presentation_survey_path'
     end
@@ -35,14 +35,14 @@ class QuestionsControllerTest < ActionController::TestCase
     it 'should allow admin to update questions' do
       updated_prompt = 'Feedback is an internal application that allows?'
 
-      patch :update, params: {
+      patch(:update, params: {
         presentation_id: @presentation.id,
         survey_id: @survey.id,
         id: @question.id,
         question: {
           prompt: updated_prompt
         }
-      }
+      })
 
       @question.reload
 
@@ -52,14 +52,14 @@ class QuestionsControllerTest < ActionController::TestCase
     it 'should redirect to correct path after an update' do
       updated_prompt = 'Feedback is an internal application that allows?'
 
-      patch :update, params: {
+      patch(:update, params: {
         presentation_id: @presentation.id,
         survey_id: @survey.id,
         id: @question.id,
         question: {
           prompt: updated_prompt
         }
-      }
+      })
 
       assert_redirected_to presentation_survey_path(@presentation.id, @survey.id), 'No redirection to presentation_survey_path'
     end
