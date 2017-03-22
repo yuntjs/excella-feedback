d19c3b1cb6001d14a286c958a24f67f644ac2758
Nick Oki <nick.oki@live.com>
2017-03-22 15:58:37 -0400

    Write unit test for SurveyTemplate#show

--- test/controllers/survey_templates_controller_test.rb
+++ test/controllers/survey_templates_controller_test.rb
@@ -7,11 +7,26 @@ class SurveyTemplatesControllerTest < ActionController::TestCase
     it 'gets all survey_templates if you are an admin' do
       admin = create :user, :admin
       survey_template = create :survey_template
+
       sign_in(admin)
 
       get(:index)
 
       assert_equal(SurveyTemplate.all, [survey_template], 'Did not return survey_templates')
+    end
+  end
+
+  describe '#show' do
+    it 'gets specific survey_template if you are an admin' do
+      admin = create :user, :admin
+      survey_template = create :survey_template
+
+      sign_in(admin)
+
+      get(:show, params: { id: survey_template.id })
+      survey_template_instance = assigns(:survey_template)
+
+      assert_equal(survey_template_instance, survey_template, 'Did not return survey_templates')
     end
   end
 end
