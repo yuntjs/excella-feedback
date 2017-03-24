2a35f6b9413da395e34ea14e59c9d1525311459d
Drew Nickerson <dnick13@gmail.com>
2017-03-24 11:52:12 -0400

    Unit tests for #required_check

--- test/helpers/survey_templates_helper_test.rb
+++ test/helpers/survey_templates_helper_test.rb
@@ -11,4 +11,19 @@ class SurveyTemplatesHelperTest < ActionView::TestCase
       assert link_string.include?(edit_survey_template_path(survey_template)), 'Link does not include correct path'
     end
   end
+
+  describe '#required_check' do
+    it 'checks if a questions_template is required and returns a formatted response' do
+      question_required = create(:question_template, :text, :required)
+      question_not_required = create(:question_template, :text, :optional)
+      expected_required = "✓"
+      expected_not_required = ""
+
+      actual_required = required_check(question_required)
+      actual_not_required = required_check(question_not_required)
+
+      assert_equal expected_required, actual_required
+      assert_equal expected_not_required, actual_not_required
+    end
+  end
 end
