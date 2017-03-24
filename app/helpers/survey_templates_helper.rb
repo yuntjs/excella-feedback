5a77eb8f09f49d4a15db68e7df426bb668c1e535
Nick Oki <nick.oki@live.com>
2017-03-24 10:54:48 -0400

    Add Edit and Delete buttons to SurveyTemplates#show

--- app/helpers/survey_templates_helper.rb
+++ app/helpers/survey_templates_helper.rb
@@ -11,4 +11,13 @@ module SurveyTemplatesHelper
 
     edit_link + delete_link
   end
+
+  def survey_template_options
+    content_tag :div, class: 'admin-options' do
+      edit_link = link_to 'Edit Survey', edit_survey_template_path(@survey_template), class: 'btn btn-primary'
+      delete_link = link_to 'Delete Survey', survey_template_path(@survey_template), class: 'btn btn-danger', method: :delete, data: { confirm: 'Are you sure?' }
+
+      edit_link + delete_link
+    end
+  end
 end
