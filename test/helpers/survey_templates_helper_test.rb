e180b865f5b98dd6ba9436fb5842f826df8f9ad8
Drew Nickerson <dnick13@gmail.com>
2017-03-24 11:31:27 -0400

    Unit tests for #new_survey_template_link

--- test/helpers/survey_templates_helper_test.rb
+++ test/helpers/survey_templates_helper_test.rb
@@ -11,4 +11,13 @@ class SurveyTemplatesHelperTest < ActionView::TestCase
       assert link_string.include?(edit_survey_template_path(survey_template)), 'Link does not include correct path'
     end
   end
+
+  describe '#new_survey_template_link' do
+    it 'shows new actions for survey templates' do
+      link_string = new_survey_template_link
+
+      assert link_string.include?('New'), 'Link does not include "New"'
+      assert link_string.include?(new_survey_template_path), 'Link does not include correct path'
+    end
+  end
 end
