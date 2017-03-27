e4202a5d63144d47f575f5503601893e53c7938d
Pramod Jacob <psjacob68@gmail.com>
2017-03-27 16:10:50 -0400

    removed unnecessary code from question_helper concern

--- app/models/concerns/question_helper.rb
+++ app/models/concerns/question_helper.rb
@@ -8,15 +8,5 @@ module QuestionHelper
     validates :prompt, presence: true
     validates :response_type, presence: true
     validates :response_required, inclusion: { in: [true, false] }
-
-    before_create :lowercase_response_type
-    before_update :lowercase_response_type
-  end
-
-  #
-  # Change response type so that it is all lowercase letters
-  #
-  def lowercase_response_type
-    response_type.downcase!
   end
 end
