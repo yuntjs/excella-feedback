57db182c0b3b81fa383b37e228944113469b0228
Pramod Jacob <psjacob68@gmail.com>
2017-03-28 11:12:36 -0400

    configued database.yml so that production environment can run on local macine

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -1,1 +1,8 @@
-puts 'Nothing to see here...'
+puts 'Creating an admin...'
+User.create(
+  first_name: 'Excella',
+  last_name: 'Admin',
+  password: '3xc311@-@dm1n',
+  is_admin: true,
+  email: 'admin@excella.com'
+)
