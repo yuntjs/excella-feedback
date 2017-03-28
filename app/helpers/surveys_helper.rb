b5aff946901e226e56f0f8b681f1eca1a363eddf
Drew Nickerson <dnick13@gmail.com>
2017-03-28 12:16:45 -0400

    New Survey button appears for presenter

--- app/helpers/surveys_helper.rb
+++ app/helpers/surveys_helper.rb
@@ -6,7 +6,9 @@ module SurveysHelper
   # Renders options/links for Presentation show page
   #
   def survey_admin_options(user, presentation)
-    return unless user.is_admin
+    participation = Participation.where(user_id: user.id, presentation_id: presentation.id).first
+    
+    return unless user.is_admin || participation.is_presenter
     content_tag :div, class: 'admin-options' do
       new_survey_link = link_to 'Create New Survey', new_presentation_survey_path(presentation), class: 'btn btn-primary'
       new_survey_link
