291923e6a240dd8b03f739877cbc1f594330d53d
Drew Nickerson <dnick13@gmail.com>
2017-03-28 12:55:17 -0400

    Presenter cannot change another presenters survey

--- app/helpers/surveys_helper.rb
+++ app/helpers/surveys_helper.rb
@@ -7,7 +7,7 @@ module SurveysHelper
   #
   def survey_admin_options(user, presentation)
     participation = Participation.where(user_id: user.id, presentation_id: presentation.id).first
-    
+
     return unless user.is_admin || participation.is_presenter
     content_tag :div, class: 'admin-options' do
       new_survey_link = link_to 'Create New Survey', new_presentation_survey_path(presentation), class: 'btn btn-primary'
@@ -24,6 +24,23 @@ module SurveysHelper
       delete_link = link_to 'Delete Survey', presentation_survey_path(@presentation, @survey), class: 'btn btn-danger', method: :delete, data: { confirm: 'Are you sure you want to delete this item?' }
 
       edit_link + delete_link
+    end
+  end
+
+  def survey_index_options(survey)
+    content_tag :div, class: 'admin-options' do
+      edit_link = link_to 'Edit Survey', edit_presentation_survey_path(@presentation, survey), class: "btn btn-primary #{disable_check(survey, current_user)}"
+      delete_link = link_to 'Delete Survey', presentation_survey_path(@presentation, survey), class: "btn btn-danger #{disable_check(survey, current_user)}", method: :delete, data: { confirm: 'Are you sure you want to delete this item?' }
+
+      edit_link + delete_link
+    end
+  end
+
+  def disable_check(survey, user)
+    if survey.presenter_id.nil? || user.is_admin || survey.presenter_id == user.id
+      return
+    else
+      "disabled disabled-link"
     end
   end
 end
