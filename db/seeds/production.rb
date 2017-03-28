a3e08a3882833ef3e60b3f1816d012e33be4ab52
Pramod Jacob <psjacob68@gmail.com>
2017-03-28 11:18:53 -0400

    minor refactoring

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -2,7 +2,7 @@ puts 'Creating an admin...'
 User.create(
   first_name: 'Excella',
   last_name: 'Admin',
-  password: '3xc311@-@dm1n',
+  password: ENV['admin_password'],
   is_admin: true,
   email: 'admin@excella.com'
 )
