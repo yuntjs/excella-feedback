557c4794c1c81d8734de28014e71d40e82868bc8
Drew Nickerson <dnick13@gmail.com>
2017-03-29 12:28:51 -0400

    Added nav class so can select with capybara for testing

---
+++ test/features/surveys/view_survey_test.rb
@@ -0,0 +1,53 @@
+require 'test_helper'
+
+class DestroySurveyTest < Capybara::Rails::TestCase
+  include Warden::Test::Helpers
+  Warden.test_mode!
+
+  after do
+    Warden.test_reset!
+  end
+
+  feature 'Show' do
+    scenario 'admin can view surveys for a presentation' do
+      admin = create(:user, :admin)
+      presentation = create(:presentation, title: 'Intro to Git')
+      survey = create(:survey, presentation_id: presentation.id)
+
+      login_as(admin, scope: :user)
+
+      visit(root_path)
+
+      within('.navbar-left') do
+        click_on('View Presentations')
+      end
+      click_on(presentation.title)
+      click_on('View Surveys')
+
+      assert_equal(presentation_surveys_path(presentation), current_path)
+    end
+
+    scenario 'presenter can view surveys for a presentation' do
+      presenter = create(:user)
+      presentation = create(:presentation, title: 'Intro to Git')
+      create(:participation, :presenter,
+             user_id: presenter.id,
+             presentation_id: presentation.id)
+      survey = create(:survey, presentation_id: presentation.id)
+
+      login_as(presenter, scope: :user)
+
+      visit(root_path)
+
+      within('.navbar-left') do
+        click_on('View Presentations')
+      end
+      click_on(presentation.title)
+      save_and_open_page
+
+      # click_on('View Surveys')
+
+      assert_equal(presentation_surveys_path(presentation), current_path)
+    end
+  end
+end
