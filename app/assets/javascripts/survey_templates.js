3cf7d627f6810cc0b629f99dca9ac5e71af52faa
Pramod Jacob <psjacob68@gmail.com>
2017-03-29 11:15:40 -0400

    added dropdown functionality to survey template display

---
+++ app/assets/javascripts/survey_templates.js
@@ -0,0 +1,25 @@
+$(function () {
+  var changeGlyphicon = function changeGlyphicon($glyphicon) {
+    if ($glyphicon.hasClass('glyphicon-chevron-right')) {
+      $glyphicon.removeClass('glyphicon-chevron-right');
+      $glyphicon.addClass('glyphicon-chevron-down');
+    } else {
+      $glyphicon.removeClass('glyphicon-chevron-down');
+      $glyphicon.addClass('glyphicon-chevron-right');
+    };
+  };
+
+  //
+  // Toggle question template data when glyphicon is clicked
+  // Change glyphicon
+  // Show & hide question templates
+  //
+  $('.survey-template-presentation .glyphicon').on('click', function(e) {
+    var templateId = $(e.target).data('id');
+    var $glyphicon = $('.glyphicon[data-id=' + templateId + ']');
+    var $questionTable = $('.question-table[data-id=' + templateId + ']');
+
+    changeGlyphicon($glyphicon);
+    $questionTable.toggle();
+  })
+})
