b319a2d63b695793ba038f72013b4e9047f70fb1
Pramod Jacob <psjacob68@gmail.com>
2017-03-30 14:05:49 -0400

    added pending tests for survey generations controller

--- test/controllers/survey_generations_controller_test.rb
+++ test/controllers/survey_generations_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
-describe SurveyGenerationsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+class SurveyGenerationsControllerTest < ActionController::TestCase
+  include Devise::Test::ControllerHelpers
+
+  before do
+    
+  end
+
+  describe '#create' do
+    it 'saves a valid survey generated from a survey template' do
+
+    end
+
+    it 'saves valid questions generated from question templates'
+
+    it 'does not save an invalid survey generated from a survey template'
+
+    it 'does not save invalid questions generated from question templates'
+  end
 end
