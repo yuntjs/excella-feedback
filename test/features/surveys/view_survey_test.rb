cbd2f5afd50f45eabc9ce8f5babf260047eaada4
Drew Nickerson <dnick13@gmail.com>
2017-03-30 11:10:46 -0400

    Added feature test to Survey create for participant

--- test/features/surveys/view_survey_test.rb
+++ test/features/surveys/view_survey_test.rb
@@ -48,5 +48,25 @@ class DestroySurveyTest < Capybara::Rails::TestCase
 
       assert_equal(presentation_surveys_path(presentation), current_path)
     end
+
+    scenario 'general participant cannot view surveys' do
+      participant = create(:user)
+      presentation = create(:presentation, :in_the_future)
+      create(:participation,
+             user_id: participant.id,
+             presentation_id: presentation.id)
+      create(:survey, presentation_id: presentation.id)
+
+      login_as(participant, scope: :user)
+
+      visit(root_path)
+
+      within('.navbar-left') do
+        click_on('View Presentations')
+      end
+      click_on(presentation.title)
+
+      refute(page.has_content?('See Surveys'))
+    end
   end
 end
