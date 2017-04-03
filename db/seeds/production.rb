d5a3697f24ef2e46101aa22fd0e7adfb385f2e77
Pramod Jacob <psjacob68@gmail.com>
2017-04-03 11:46:52 -0400

    reconfigured production seeds file

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -1,8 +1,13 @@
-puts 'Creating an admin...'
-User.create(
-  first_name: 'Excella',
-  last_name: 'Admin',
-  password: ENV['admin_password'],
-  is_admin: true,
-  email: 'admin@excella.com'
-)
+require './db/seeds/admins.rb'
+
+puts 'Setting initial admins...'
+ADMINS.each do |admin|
+  user = User.new(
+    first_name: admin[:first_name],
+    last_name: admin[:last_name],
+    password: ENV['ADMIN_PASS_INIT'],
+    is_admin: true
+  )
+  user.email = "#{admin[:first_name]}.#{admin[:last_name]}@excella.com"
+  user.save
+end
