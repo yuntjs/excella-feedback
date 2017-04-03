9662ef89e424c112f87a9d584c89b45e8c0eae7c
Pramod Jacob <psjacob68@gmail.com>
2017-04-03 11:57:23 -0400

    modified seeds file

---
+++ db/seeds/admins.rb
@@ -0,0 +1,6 @@
+ADMINS = [
+  { first_name: 'Nicholas', last_name: 'Oki' },
+  { first_name: 'Pramod', last_name: 'Jacob' },
+  { first_name: 'Khoi', last_name: 'Le' },
+  { first_name: 'Drew', last_name: 'Nickerson' }
+]
