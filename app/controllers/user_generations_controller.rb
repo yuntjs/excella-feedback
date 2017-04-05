f40174b445f94463d69dfdca2927f9a927ecafe2
Pramod Jacob <psjacob68@gmail.com>
2017-04-05 13:47:06 -0400

    added user generations controller & routes

---
+++ app/controllers/user_generations_controller.rb
@@ -0,0 +1,17 @@
+#
+# UserGenerationsController
+#
+class UserGenerationsController < ApplicationController
+  #
+  # New
+  #
+  def new
+    
+  end
+
+  #
+  # Create
+  #
+  def create
+  end
+end
