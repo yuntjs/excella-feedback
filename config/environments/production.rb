55a9cb67098228efc16ba2cb93735e086f850f4e
Pramod Jacob <psjacob68@gmail.com>
2017-04-06 16:31:57 -0400

    added default url option for action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,4 +69,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Configure Devise default URL
+  config.action_mailer.default_url_options = { host: 'excella-feedback.herokuapp.com' }
 end
