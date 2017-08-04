2a2cf482c4af53a38e438a28acae32a2b39855dc
Tae Yun <yuntjs@gmail.com>
2017-08-04 15:59:05 -0400

    adds pem

--- deploy/travis-deploy.sh
+++ deploy/travis-deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-ssh -i ../tjy-west-1.pem -o "StrictHostKeyChecking no" ubuntu@$AWS_IP env \
+echo "****************************************"
+pwd
+ssh -i ~/tjy-west-1.pem -o "StrictHostKeyChecking no" ubuntu@$AWS_IP env \
 TAG=0.1.$TRAVIS_BUILD_NUMBER \
 DOCKER_USER=$DOCKER_USER \
 DOCKER_PASS=$DOCKER_PASS \
